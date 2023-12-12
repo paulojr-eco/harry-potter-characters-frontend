2d2b3df57d56fcd786da154ca915cfb506e61b5f
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-12 14:40:23 -0300

    feat: add agrorder filter and change favico

--- src/app/components/character-card/character-card.component.ts
+++ src/app/components/character-card/character-card.component.ts
@@ -10,4 +10,16 @@ export class CharacterCardComponent {
   @Input() character!: Character;
 
   constructor() {}
+
+  calculateAge(dateOfBirth: string): string {
+    const birthDateParts = dateOfBirth.split('-').map(part => parseInt(part, 10));
+    const birthDate = new Date(birthDateParts[2], birthDateParts[1] - 1, birthDateParts[0]);
+    const now = new Date();
+    let age = now.getFullYear() - birthDate.getFullYear();
+    const months = now.getMonth() - birthDate.getMonth();
+    if (months < 0 || (months === 0 && now.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age.toString();
+  }
 }
--- src/app/components/character-filter/character-filter.component.ts
+++ src/app/components/character-filter/character-filter.component.ts
@@ -9,12 +9,8 @@ import { CharactersService } from '../../services/characters/characters.service'
 })
 export class CharacterFilterComponent {
   private characters: Character[] = [];
-  houses: string[] = [
-    'Gryffindor',
-    'Slytherin',
-    'Ravenclaw',
-    'Hufflepuff',
-  ];
+  houses: string[] = ['Gryffindor', 'Slytherin', 'Ravenclaw', 'Hufflepuff'];
+  ageOrders: string[] = ['Crescente', 'Decrescente'];
 
   constructor(private charactersService: CharactersService) {
     this.charactersService.getCharacters().subscribe((characters) => {
@@ -22,7 +18,7 @@ export class CharacterFilterComponent {
     });
   }
 
-  applyFilters(name: string, house: string) {
+  applyFilters(name: string, house: string, ageOrder: string) {
     let filteredCharacters = this.characters;
     if (name !== '') {
       filteredCharacters = filteredCharacters.filter((character) =>
@@ -33,6 +29,52 @@ export class CharacterFilterComponent {
       filteredCharacters = filteredCharacters.filter(
         (character) => character.house === house
       );
+    }
+    if (ageOrder === 'Crescente') {
+      filteredCharacters = filteredCharacters.sort((a, b) => {
+        if (!a.dateOfBirth) return 1;
+        if (!b.dateOfBirth) return -1;
+        const aBirthDateParts = a.dateOfBirth
+          .split('-')
+          .map((part) => parseInt(part, 10));
+        const aBirthDate = new Date(
+          aBirthDateParts[2],
+          aBirthDateParts[1] - 1,
+          aBirthDateParts[0]
+        );
+        const bBirthDateParts = b.dateOfBirth
+          .split('-')
+          .map((part) => parseInt(part, 10));
+        const bBirthDate = new Date(
+          bBirthDateParts[2],
+          bBirthDateParts[1] - 1,
+          bBirthDateParts[0]
+        );
+        return bBirthDate.getTime() - aBirthDate.getTime();
+      });
+    }
+    if (ageOrder === 'Decrescente') {
+      filteredCharacters = filteredCharacters.sort((a, b) => {
+        if (!a.dateOfBirth) return 1;
+        if (!b.dateOfBirth) return -1;
+        const aBirthDateParts = a.dateOfBirth
+          .split('-')
+          .map((part) => parseInt(part, 10));
+        const aBirthDate = new Date(
+          aBirthDateParts[2],
+          aBirthDateParts[1] - 1,
+          aBirthDateParts[0]
+        );
+        const bBirthDateParts = b.dateOfBirth
+          .split('-')
+          .map((part) => parseInt(part, 10));
+        const bBirthDate = new Date(
+          bBirthDateParts[2],
+          bBirthDateParts[1] - 1,
+          bBirthDateParts[0]
+        );
+        return aBirthDate.getTime() - bBirthDate.getTime();
+      });
     }
 
     this.charactersService.setFilteredCharacters(filteredCharacters);
